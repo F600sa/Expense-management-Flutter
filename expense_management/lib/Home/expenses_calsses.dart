19cded1825107beb0d5b4b513268eee149287f55
norah <norahaltamimi@norahs>
2022-10-28 13:17:01 +0300

    start point

---
+++ expense_management/lib/Home/expenses_calsses.dart
@@ -0,0 +1,77 @@
+// ignore_for_file: unnecessary_this, prefer_initializing_formals
+
+class Shopping {
+  double? price;
+  bool? isCash;
+  DateTime? date;
+  String? note;
+  String? icon;
+  Shopping(double price, bool isCash, DateTime date,String note,String icon) {
+    this.date = date;
+    this.isCash = isCash;
+    this.price = price;
+    this.note = note;
+    this.icon = icon;
+
+  }
+}
+
+class Restaurant {
+  double? price;
+  bool? isCash;
+  DateTime? date;
+  String? note;
+  String? icon;
+  Restaurant(double price, bool isCash, DateTime date,String note,String icon) {
+    this.date = date;
+    this.isCash = isCash;
+    this.price = price;
+    this.note = note;
+    this.icon = icon;
+  }
+}
+
+class Transport {
+  double? price;
+  bool? isCash;
+  DateTime? date;
+  String? note;
+  String? icon;
+  Transport(double price, bool isCash, DateTime date,String note,String icon) {
+    this.date = date;
+    this.isCash = isCash;
+    this.price = price;
+    this.note = note;
+    this.icon = icon;
+  }
+}
+
+class Bill {
+  double? price;
+  bool? isCash;
+  DateTime? date;
+  String? note;
+  String? icon;
+  Bill(double price, bool isCash, DateTime date,String note,String icon) {
+    this.date = date;
+    this.isCash = isCash;
+    this.price = price;
+    this.note = note;
+    this.icon = icon;
+  }
+}
+
+class Another {
+  double? price;
+  bool? isCash;
+  DateTime? date;
+  String? note;
+  String? icon;
+  Another(double price, bool isCash, DateTime date,String note,String icon) {
+    this.date = date;
+    this.isCash = isCash;
+    this.price = price;
+    this.note = note;
+    this.icon = icon;
+  }
+}
---
+++ expense_management/lib/Home/home_controller.dart
@@ -0,0 +1,192 @@
+// ignore_for_file: non_constant_identifier_names, avoid_print ,prefer_interpolation_to_compose_strings, unnecessary_new
+
+import 'expenses_calsses.dart';
+
+class C {
+  /* Expenses */
+  List expenses = [];
+
+  double total_expenses = 0;
+
+  void expenses_list() {
+    for (var ex in expenses) {
+      print(ex.price);
+      print(ex.isCash);
+      print(ex.date.day.toString() +
+          "-" +
+          ex.date.month.toString() +
+          "-" +
+          ex.date.year.toString());
+      print(ex.note);
+    }
+  }
+
+  double total_expenses_fun() {
+    for (var ex in expenses) {
+      total_expenses += ex.price;
+    }
+    return total_expenses;
+  }
+  /* income and  current_balance saving */
+  double income = 6000;
+
+  double spending_fun(double expenses,double goals,) {
+    return  expenses+goals;
+  }
+  double current_balance_fun(double income,double spending,) {
+    return  income-spending;
+  }
+  double saving_fun(double income,double spending,){
+    return  income-spending;
+  }
+
+/* Shopping */
+  List shopping = [];
+  double total_shopping = 0;
+
+  void add_shopping_list(
+      double price, bool isCash, DateTime date, String note, String icon) {
+    shopping.add(new Shopping(price, isCash, date, note, icon));
+    expenses.add(new Shopping(price, isCash, date, note, icon));
+  }
+
+  void shopping_list() {
+    for (var sho in shopping) {
+      print(sho.price);
+      print(sho.isCash);
+      print(sho.date.day.toString() +
+          "-" +
+          sho.date.month.toString() +
+          "-" +
+          sho.date.year.toString());
+      print(sho.note);
+    }
+  }
+
+  double total_shopping_fun() {
+    for (var sho in shopping) {
+      total_shopping += sho.price;
+    }
+    return total_shopping;
+  }
+
+/* Restaurant */
+  List restaurant = [];
+  double total_restaurant = 0;
+
+  void add_restaurant_list(
+      double price, bool isCash, DateTime date, String note, String icon) {
+    restaurant.add(new Restaurant(price, isCash, date, note, icon));
+    expenses.add(new Restaurant(price, isCash, date, note, icon));
+  }
+
+  void restaurant_list() {
+    for (var re in restaurant) {
+      print(re.price);
+      print(re.isCash);
+      print(re.date.day.toString() +
+          "-" +
+          re.date.month.toString() +
+          "-" +
+          re.date.year.toString());
+      print(re.note);
+    }
+  }
+
+  double total_restaurant_fun() {
+    for (var re in restaurant) {
+      total_restaurant += re.price;
+    }
+    return total_restaurant;
+  }
+
+/* Transport */
+  List transport = [];
+  double total_transport = 0;
+
+  void add_transport_list(
+      double price, bool isCash, DateTime date, String note, String icon) {
+    transport.add(new Transport(price, isCash, date, note, icon));
+    expenses.add(new Transport(price, isCash, date, note, icon));
+  }
+
+  void transport_list() {
+    for (var tr in transport) {
+      print(tr.price);
+      print(tr.isCash);
+      print(tr.date.day.toString() +
+          "-" +
+          tr.date.month.toString() +
+          "-" +
+          tr.date.year.toString());
+      print(tr.note);
+    }
+  }
+
+  double total_transport_fun() {
+    for (var tr in transport) {
+      total_transport += tr.price;
+    }
+    return total_transport;
+  }
+
+/* Bill */
+  List bill = [];
+  double total_bill = 0;
+
+  void add_bill_list(
+      double price, bool isCash, DateTime date, String note, String icon) {
+    bill.add(new Bill(price, isCash, date, note, icon));
+    expenses.add(new Bill(price, isCash, date, note, icon));
+  }
+
+  void bill_list() {
+    for (var bi in bill) {
+      print(bi.price);
+      print(bi.isCash);
+      print(bi.date.day.toString() +
+          "-" +
+          bi.date.month.toString() +
+          "-" +
+          bi.date.year.toString());
+      print(bi.note);
+    }
+  }
+
+  double total_bill_fun() {
+    for (var bi in bill) {
+      total_bill += bi.price;
+    }
+    return total_bill;
+  }
+
+/* Bill */
+  List another = [];
+  double total_another = 0;
+
+  void add_another_list(
+      double price, bool isCash, DateTime date, String note, String icon) {
+    another.add(new Another(price, isCash, date, note, icon));
+    expenses.add(new Another(price, isCash, date, note, icon));
+  }
+
+  void another_list() {
+    for (var at in another) {
+      print(at.price);
+      print(at.isCash);
+      print(at.date.day.toString() +
+          "-" +
+          at.date.month.toString() +
+          "-" +
+          at.date.year.toString());
+      print(at.note);
+    }
+  }
+
+  double total_another_fun() {
+    for (var at in another) {
+      total_another += at.price;
+    }
+    return total_another;
+  }
+}
