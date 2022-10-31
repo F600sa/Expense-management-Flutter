60e627602fb1f1a1d3a1c7302d83b3e9e27fa97e
norah <norahaltamimi@norahs>
2022-10-31 20:39:27 +0300

    just I changed our  file name

---
+++ lib/Transaction/add_transaction.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+class Transaction extends StatelessWidget {
+  const Transaction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        body: Text("Transaction"),
+      ),
+    );
+  }
+}
