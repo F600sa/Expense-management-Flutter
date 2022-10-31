5554346c4ab61eea98b4ad562f33cb17bf045fb5
norah <norahaltamimi@norahs>
2022-10-31 20:35:16 +0300

    here I made files structure and I navbar with routing and it is include all our pages .you guys just need to start building pages no worry about anything else

---
+++ jaeeb_arabic_version/lib/Profile/profile_screen.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        body: Text("ProfileScreen"),
+      ),
+    );
+  }
+}
---
+++ jaeeb_arabic_version/lib/Transaction/transaction_screen.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+class AddTransaction extends StatelessWidget {
+  const AddTransaction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        body: Text("AddTransaction"),
+      ),
+    );
+  }
+}
